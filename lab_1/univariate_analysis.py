ee9ef9bdb0e9f9ff0617ad5401409821b42bedee
Владислав <103439608+lilvladick@users.noreply.github.com>
2026-01-14 20:29:24 +0300

    lab 1: univariable analysis for amount and rental

---
+++ lab_1/univariate_analysis.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+sns.set_theme(style="whitegrid")
+
+df = pd.read_csv('utils/data/optimized_sakila.csv')
+
+numeric_columns = df.select_dtypes(include=["int64", "float64"]).columns
+
+# print(numeric_columns)
+
+# print(df[numeric_columns].describe())
+
+features_columns = ["rental_rate", "amount"]  # адекватные столбики (главное что не айди =) )
+
+for column in features_columns:
+    print("Stats:\n", df[column].describe())
+
+    plt.figure(figsize=(8, 5))
+    sns.histplot(
+        data=df,
+        x=column,
+        bins=20,
+        kde=True
+    )
+    plt.xlabel(column)
+    plt.ylabel("Number of meetings")
+    plt.show()
